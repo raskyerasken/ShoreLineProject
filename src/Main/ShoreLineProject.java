8c23a0a346e142716e6d68847c212223e621fc60
Sub-Z3r0 <andersen19@gmail.com>
2018-04-23 12:09:22 +0200

    created Main package

--- src/Main/ShoreLineProject.java
+++ src/Main/ShoreLineProject.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUI;
+package Main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +19,7 @@ public class ShoreLineProject extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/GUI/MainWindow.fxml"));
         
         Scene scene = new Scene(root);
         
