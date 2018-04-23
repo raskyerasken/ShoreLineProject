bb883058625113c2f271c0e36420c043b5e1936c
raskyerasken <32293919+raskyerasken@users.noreply.github.com>
2018-04-23 12:32:20 +0200

    hi

--- src/GUI/MainWindowController.java
+++ src/GUI/MainWindowController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 /**
  *
@@ -20,8 +22,23 @@ public class MainWindowController implements Initializable {
     
     @FXML
     private Label label;
-    
     @FXML
+    private Button importData;
+    @FXML
+    private Button convertData;
+    @FXML
+    private Button exportData;
+    @FXML
+    private Button startTask;
+    @FXML
+    private Button pauseTask;
+    @FXML
+    private Button stopTask;
+    @FXML
+    private Label taskXRun;
+    @FXML
+    private ListView<?> taskField;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
