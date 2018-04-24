1348f9ef87543f79ae61e344befa243e78de4793
raskyerasken <32293919+raskyerasken@users.noreply.github.com>
2018-04-24 15:34:54 +0200

    d

--- src/GUI/MainWindowController.java
+++ src/GUI/MainWindowController.java
@@ -14,14 +14,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-<<<<<<< HEAD
 import javafx.scene.control.MenuBar;
-=======
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
->>>>>>> be63f12d2b6c5d0113320364ad773d493c331e61
 
 /**
  *
@@ -54,38 +51,10 @@ public class MainWindowController implements Initializable
         menuBar.setId("MenuBar");
     }   
 
-<<<<<<< HEAD
-=======
-    @FXML
-<<<<<<< HEAD
-    private void importData(ActionEvent event) {
-    }
-
-    @FXML
-    private void convertData(ActionEvent event) {
-    }
-
-    @FXML
-    private void exportData(ActionEvent event) {
-    }
-
-    @FXML
-    private void startTask(ActionEvent event) {
-    }
-
-    @FXML
-    private void pauseTask(ActionEvent event) {
-    }
-
-    @FXML
-    private void stopTask(ActionEvent event) {
-    }
-=======
     private void importDataClick(MouseEvent event) {
         
        
     }
->>>>>>> 5f071107377ba4a8290135f2f10c863cdf4854e3
 
     private void importDataClick(ActionEvent event) {
         
@@ -109,38 +78,9 @@ public class MainWindowController implements Initializable
     
     void stageToFront()
     {
-        Stage stage = (Stage) startTask.getScene().getWindow();
+        Stage stage = (Stage) taskField.getScene().getWindow();
         stage.toFront();
-    }
 
-    @FXML
-    private void importData(ActionEvent event) {
-    }
-
-    @FXML
-    private void convertData(ActionEvent event) {
-    }
-
-    @FXML
-    private void exportData(ActionEvent event) {
-    }
-
-    @FXML
-    private void startTask(ActionEvent event) {
-    }
-
-    @FXML
-    private void pauseTask(ActionEvent event) {
-    }
-
-    @FXML
-    private void stopTask(ActionEvent event) {
-    }
     
-    
->>>>>>> be63f12d2b6c5d0113320364ad773d493c331e61
-   
-    
-    
-  
+    }
 }
