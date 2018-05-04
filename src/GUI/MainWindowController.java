496d77c1206f806ef1fa6783c56c9728c9bc9266
Sub-Z3r0 <andersen19@gmail.com>
2018-05-04 11:35:14 +0200

    pushy

--- src/GUI/MainWindowController.java
+++ src/GUI/MainWindowController.java
@@ -7,10 +7,7 @@ package GUI;
 
 import BE.JSON;
 import BE.UpdateLog;
-<<<<<<< HEAD
 import BLL.BLLManagerUpdateLog;
-=======
->>>>>>> ac603c994986911ed75605def1356e357a511c25
 import BLL.CreateJSONFile;
 import BLL.ReadingXLSX;
 import GUI.TEST.XmlToJava;
@@ -54,13 +51,10 @@ import org.xml.sax.SAXException;
  *
  * @author Jason and Freddy Kruger
  */
-public class MainWindowController implements Initializable {
-<<<<<<< HEAD
+public class MainWindowController implements Initializable 
+{
     
     LoginViewController loginID;
-=======
-
->>>>>>> ac603c994986911ed75605def1356e357a511c25
     boolean acceptfile = false;
     String[] acceptetFiles = {".xlsx"};
     List<File> files;
@@ -74,33 +68,25 @@ public class MainWindowController implements Initializable {
     private AnchorPane importWindow;
     @FXML
     private Button importbtn;
+    private final ObservableList<File> filesAcceptet
+            = FXCollections.observableArrayList();
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        importbtn.setStyle("-fx-background-color: #588fe8;");
+    }
+    
     private void activateXmlReader() {
 
     }
 
-    private void handleButtonAction(ActionEvent event) {
+    private void importDataClick(MouseEvent event)
+    {
 
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-<<<<<<< HEAD
 
-=======
-importbtn.setStyle("-fx-background-color: #588fe8;");
->>>>>>> ac603c994986911ed75605def1356e357a511c25
-    }
-
-    private void importDataClick(MouseEvent event) {
-
-    }
-<<<<<<< HEAD
-    List<File> filesAcceptet = new ArrayList<>();
-=======
-     private final ObservableList<File> filesAcceptet 
-            = FXCollections.observableArrayList();
->>>>>>> ac603c994986911ed75605def1356e357a511c25
 
     @FXML
     private void importData(ActionEvent event) throws SAXException, IOException, ParseException, IllegalArgumentException, IllegalAccessException, SQLException {
@@ -108,19 +94,14 @@ importbtn.setStyle("-fx-background-color: #588fe8;");
         fileChooser.setTitle("Open Image File");
         fileChooser.setInitialDirectory(new File("..."));
         files = fileChooser.showOpenMultipleDialog(new Stage());
-<<<<<<< HEAD
         UpdateLog updateLog = new UpdateLog();
         BLL.BLLManagerUpdateLog up = new BLLManagerUpdateLog();
         
-=======
-
->>>>>>> ac603c994986911ed75605def1356e357a511c25
         for (File file : files) {
             for (String acceptetFile : acceptetFiles) {
                 if (file.getAbsolutePath().endsWith(acceptetFile)) {
                     filesAcceptet.add(file);
                     acceptfile = true;
-<<<<<<< HEAD
                     
                     Timestamp currentTimestamp = new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
                     java.sql.Timestamp sqlDate = new java.sql.Timestamp(currentTimestamp.getTime());
@@ -131,8 +112,6 @@ importbtn.setStyle("-fx-background-color: #588fe8;");
                     up.setUpdateLog(updateLog);
                     
                     System.out.println("writes");
-=======
->>>>>>> ac603c994986911ed75605def1356e357a511c25
 
                 }
                 if (!acceptfile) {
@@ -141,21 +120,8 @@ importbtn.setStyle("-fx-background-color: #588fe8;");
                             = new AlertWindow("File not support yet", null, "This file " + file.getAbsolutePath() + " can be added");
                 }
             }
-<<<<<<< HEAD
-
-=======
             taskField.setItems((ObservableList<File>) filesAcceptet);
->>>>>>> ac603c994986911ed75605def1356e357a511c25
         }
-
-        for (int i = 0; i < filesAcceptet.size(); i++) {
-            ReadingXLSX XLSX = new ReadingXLSX(filesAcceptet.get(i).getAbsolutePath());
-            XLSX.allRows();
-            XLSX.getColumsNames();
-            CreateJSONFile createJSON = new CreateJSONFile();
-            createJSON.createJSON(XLSX.allJSONObjectInFile(), filesAcceptet.get(i).getName());
-        }
-
     }
 
     @FXML
